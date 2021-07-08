8de95bdd60eefdc7b2ac1a6a60f66574d0dd9c77
Nashiruddien Sadid Mustaqim <69621862+diditsadidnsm@users.noreply.github.com>
2021-07-08 19:37:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var serveStatic = require('serve-static');
 
 var app = express();
-app.use(serveStatic(__dirname + "index.html"));
+app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);

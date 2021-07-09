187e560e6389c9c5033f4134b8c476eb6334889b
Nashiruddien Sadid Mustaqim <69621862+diditsadidnsm@users.noreply.github.com>
2021-07-09 11:11:16 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+let publicPath = process.env.NODE_ENV === 'production' ? 'http://188.166.181.108:5000/' : '/';
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production',
-  productionSourceMap: true,
+  publicPath,
+  productionSourceMap: false,
 };
+

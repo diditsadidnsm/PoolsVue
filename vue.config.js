02297e469223813c8a2dd87577776eb3555e2a1b
Nashiruddien Sadid Mustaqim <69621862+diditsadidnsm@users.noreply.github.com>
2021-07-09 10:56:14 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-let publicPath = process.env.NODE_ENV === 'production' ? 'PoolsVue' : '/';
+let publicPath = process.env.NODE_ENV === 'production' ? 'http://188.166.181.108:5000/#/login' : '/';
 
 module.exports = {
   publicPath,
